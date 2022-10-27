b0a3cdd0469d7b23c006300958462669cb5894d3
ramosw1993 <ramosw1993@gmail.com>
2022-10-27 11:43:07 -0400

    update

--- models/user.js
+++ models/user.js
@@ -1,12 +1,7 @@
-const { Model, DataTypes } = require('sequelize');
-const bcrypt = require('bcrypt');
+const {Sequelize, Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class User extends Model {
-  checkPassword(loginPw) {
-    return bcrypt.compareSync(loginPw, this.password);
-  }
-}
+class User extends Model {}
 
 User.init(
   {
@@ -17,6 +12,10 @@ User.init(
       autoIncrement: true,
     },
     name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    currentJob: {
       type: DataTypes.STRING,
       allowNull: false,
     },
@@ -35,24 +34,12 @@ User.init(
         len: [8],
       },
     },
-  },
-  {
-    hooks: {
-      beforeCreate: async (newUserData) => {
-        newUserData.password = await bcrypt.hash(newUserData.password, 10);
-        return newUserData;
-      },
-      beforeUpdate: async (updatedUserData) => {
-        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
-        return updatedUserData;
-      },
-    },
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
     modelName: 'user',
-  }
+  },
 );
 
 module.exports = User;
