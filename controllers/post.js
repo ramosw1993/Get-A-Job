17bbdc7ed302a22c7eff4f6923910c0fe2d5b09f
ramosw1993 <ramosw1993@gmail.com>
2022-10-27 12:56:43 -0400

    update hannah

--- controllers/post.js
+++
@@ -1,9 +0,0 @@
-
-const post = require('express').Router();
-// const { User } = require('../models');
-
-// This is the 'get' route for post
-post.get('/', async (req, res) => {
-});
-
-module.exports = post;
