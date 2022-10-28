ea4d31aeb1b415d85bd4c52d1179a7b90a27364b
unknown <alicia9790972@gmail.com>
2022-10-28 14:03:10 -0400

    css and posts

---
+++ seeds/seed.js
@@ -0,0 +1,23 @@
+const sequelize = require("../config/connection");
+const { User, Post, Comment } = require("../models");
+
+const userData = require("./userData.json");
+const postData = require("./postData.json");
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  await Post.bulkCreate(postData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  process.exit(0);
+};
+
+seedDatabase();
