4c5abc33a76d9ec4e6374de78979e8274d5a0921
unknown <alicia9790972@gmail.com>
2022-10-28 11:48:59 -0400

    profie route

--- controllers/profileRoutes.js
+++ controllers/profileRoutes.js
@@ -3,22 +3,20 @@ const { Post, User } = require("../models");
 const { findAll } = require("../models/User");
 const withAuth = require("../utils/auth");
 
-router.get("/profile", withAuth, async (req, res) => {
-    try {
-      const userPosts = await Post.findAll({
-        where: { ${req.user.id} },
-        raw: true,
-      });
-      console.log(userPosts);
-      res.render("profile", {
-        userPosts,
-        ${req.user.id},
-      });
-    } catch (err) {
-      res.status(400).json(err);
-    }
+router.get("/", withAuth, async (req, res) => {
+  try {
+    const userPosts = await Post.findAll({
+      where: { user_id: req.session.user_id },
+      raw: true,
+    });
+    console.log(userPosts);
+    res.render("profile", {
+      userPosts,
+      user_id: req.session.user_id,
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
-
-
 
 module.exports = router;
