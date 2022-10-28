989db083f370c79ad1c1bc2e1d3d2c28c1291311
unknown <alicia9790972@gmail.com>
2022-10-28 11:40:31 -0400

    profile

--- controllers/profileRoutes.js
+++ controllers/profileRoutes.js
@@ -1,38 +1,24 @@
 const router = require("express").Router();
-const { Comment, Post, User } = require("../models");
+const { Post, User } = require("../models");
 const { findAll } = require("../models/User");
 const withAuth = require("../utils/auth");
 
-router.get("/:id", withAuth, async (req, res) => {
-  try {
-    const postData = await Post.findByPk(req.params.id, {
-      include: [
-        {
-          model: User,
-          attributes: ["username"],
-        },
-      ],
-      raw: true,
-      nest: true,
-    });
-
-    const commentData = await Comment.findAll({
-      where: {
-        post_id: req.params.id,
-      },
-      raw: true,
-    });
-
-    console.log(postData, commentData);
-
-    res.render("postdetail", {
-      commentData,
-      postData,
-      logged_in: req.session.logged_in,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
+router.get("/profile", withAuth, async (req, res) => {
+    try {
+      const userPosts = await Post.findAll({
+        where: { ${req.user.id} },
+        raw: true,
+      });
+      console.log(userPosts);
+      res.render("profile", {
+        userPosts,
+        ${req.user.id},
+      });
+    } catch (err) {
+      res.status(400).json(err);
+    }
 });
+
+
 
 module.exports = router;
