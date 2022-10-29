e0d0c8643a7d50911d2a770fc132979c869e7ce4
unknown <alicia9790972@gmail.com>
2022-10-29 05:01:48 -0400

    started edit profile routes and handlebars

--- controllers/profileRoutes.js
+++ controllers/profileRoutes.js
@@ -17,9 +17,16 @@ router.get("/", withAuth, async (req, res) => {
       ],
     });
     console.log(userPosts);
+
+    if (userPosts[0].user_id === req.user.id) {
+      thisUser = true;
+    } else {
+      thisUser = false;
+    }
+
     res.render("profile", {
       userPosts,
-
+      thisUser,
       user_id: req.user.id,
     });
   } catch (err) {
@@ -29,6 +36,7 @@ router.get("/", withAuth, async (req, res) => {
 
 router.get("/:id", withAuth, async (req, res) => {
   try {
+    let thisUser;
     const userPosts = await Post.findAll({
       where: { user_id: req.params.id },
       raw: true,
@@ -41,9 +49,17 @@ router.get("/:id", withAuth, async (req, res) => {
       ],
     });
     console.log(userPosts);
+    if (userPosts[0].user_id === req.user.id) {
+      thisUser = true;
+    } else {
+      thisUser = false;
+    }
+
+    console.log(thisUser);
+
     res.render("profile", {
       userPosts,
-
+      thisUser,
       user_id: req.user.id,
     });
   } catch (err) {
---
+++ public/js/editProfile.js
@@ -0,0 +1,51 @@
+function openMenu(event) {
+  event.preventDefault();
+
+  const formHolder = document.getElementById("formHolder");
+  const jobInput = document.createElement("input");
+
+  jobInput.setAttribute("class", "form-control");
+  jobInput.setAttribute("type", "text");
+  jobInput.setAttribute("placeholder", "Job Input");
+  jobInput.setAttribute("id", "jobInput");
+
+  const imgInput = document.createElement("input");
+
+  imgInput.setAttribute("class", "form-control");
+  imgInput.setAttribute("type", "text");
+  imgInput.setAttribute("placeholder", "Image Link Input");
+  imgInput.setAttribute("id", "imgInput");
+
+  const submitBtn = document.createElement("button");
+
+  submitBtn.setAttribute("id", "submitBtn");
+  submitBtn.innerText = "Submit";
+
+  formHolder.appendChild(jobInput);
+  formHolder.appendChild(imgInput);
+  formHolder.appendChild(submitBtn);
+
+  document.querySelector("#submitBtn").addEventListener("click", editProfile);
+}
+
+async function editProfile(event) {
+  event.preventDefault();
+
+  const current_job = document.getElementById("jobInput").value.trim();
+  const profile_pic = document.getElementById("imgInput").value.trim();
+
+  const res = await fetch(`/api/users/edit/${userId}`, {
+    method: "POST",
+    body: JSON.stringify({ current_job, profile_pic }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  if (res.ok) {
+    document.location.replace("/profile");
+  } else {
+    alert("Failed to update!");
+  }
+}
+
+document.querySelector("#editMenu").addEventListener("click", openMenu);
