fd67cd43a1f590f09739d789ddc7ad51e3ec079d
ramosw1993 <ramosw1993@gmail.com>
2022-10-29 16:57:31 -0400

    update dashboard controller

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -4,21 +4,17 @@ async function newFormHandler(event) {
     const title = document.querySelectorAll('#post-title').value;
     const description = document.querySelectorAll('#post-description').value;
     const pay = document.querySelectorAll('post-pay').value;
-    const date = document.querySelectorAll('#post-date').value;
+    const date = document.querySelectorAll('#post-date').value; 
 
-    const id = window.location.toString().split('/')[
-        window.location.toString().split('/').length - 1
-      ];    
-
-    const res = await fetch(`/api/post/${id}`, {
-        method: 'PUT',
+    const res = await fetch(`/api/post`, {
+        method: 'POST',
         body: JSON.stringify({title, description, pay, date}),
         headers: {
             "Content-Type": "application/json"
         },
     });
     if (res.ok) {
-        document.location.replace(`/api/post/${id}`);
+        document.location.replace(`/dashboard`);
     } else {
         alert("Failed to create post!");
     }
