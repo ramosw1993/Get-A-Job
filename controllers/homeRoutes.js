359154e7283a5ab7df19bffbe133a9fe9e8ec3a8
Pak Yin Sin <Spy@MacBook-Pro.local>
2022-10-31 13:09:50 -0400

    update google cbURL

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -11,7 +11,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:3001/google/callback",
+      callbackURL: "https://get-a-job.herokuapp.com/google/callback",
       passReqToCallback: true,
     },
     async function (req, accessToken, refreshToken, profile, done) {
